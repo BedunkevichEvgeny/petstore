34645dff2bcd8744f27d3268dcc55977ddfbb9ed
Yauhen Bedunkevich <yauhen_bedunkevich@epam.com>
2026-03-25 01:25:05 +0300

    add exception throw for app insights

--- petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/ProductManagementService.java
+++ petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/service/ProductManagementService.java
@@ -44,39 +44,42 @@ public class ProductManagementService {
                 requestId, traceId, category);
 
         try {
-            this.sessionUser.getTelemetryClient().trackEvent(
-                    String.format("PetStoreApp user %s is requesting to retrieve products from the ProductService",
-                            this.sessionUser.getName()),
-                    this.sessionUser.getCustomEventProperties(), null);
+            throw new RuntimeException("Cannot move further");
 
-            products = productServiceClient.getProductsByStatus(AVAILABLE.getValue());
-            this.sessionUser.setProducts(products);
+//            this.sessionUser.getTelemetryClient().trackEvent(
+//                    String.format("PetStoreApp user %s is requesting to retrieve products from the ProductService",
+//                            this.sessionUser.getName()),
+//                    this.sessionUser.getCustomEventProperties(), null);
+//
+//            products = productServiceClient.getProductsByStatus(AVAILABLE.getValue());
+//            this.sessionUser.setProducts(products);
+//
+//            if (tags.stream().anyMatch(t -> t.getName().equals("large"))) {
+//                products = products.stream()
+//                        .filter(product -> category.equals(product.getCategory().getName())
+//                                && product.getTags().toString().contains("large"))
+//                        .toList();
+//            } else {
+//                products = products.stream()
+//                        .filter(product -> category.equals(product.getCategory().getName())
+//                                && product.getTags().toString().contains("small"))
+//                        .toList();
+//            }
+//
+//            log.info("Successfully retrieved {} products for category {} with tags {} [RequestID: {}, TraceID: {}]",
+//                    products.size(), category, tags, requestId, traceId);
+//
+//            this.sessionUser.getTelemetryClient().trackEvent(
+//                "Amount of products",
+//                this.sessionUser.getCustomEventProperties(),
+//                Map.of("amount", Integer.valueOf(products.size()).doubleValue())
+//            );
+//
+//            log.info("Custom Successfully retrieved {} products for category {} with tags {} [RequestID: {}, TraceID: {}]",
+//                products.size(), category, tags, requestId, traceId);
 
-            if (tags.stream().anyMatch(t -> t.getName().equals("large"))) {
-                products = products.stream()
-                        .filter(product -> category.equals(product.getCategory().getName())
-                                && product.getTags().toString().contains("large"))
-                        .toList();
-            } else {
-                products = products.stream()
-                        .filter(product -> category.equals(product.getCategory().getName())
-                                && product.getTags().toString().contains("small"))
-                        .toList();
-            }
 
-            log.info("Successfully retrieved {} products for category {} with tags {} [RequestID: {}, TraceID: {}]",
-                    products.size(), category, tags, requestId, traceId);
-
-            this.sessionUser.getTelemetryClient().trackEvent(
-                "Amount of products",
-                this.sessionUser.getCustomEventProperties(),
-                Map.of("amount", Integer.valueOf(products.size()).doubleValue())
-            );
-
-            log.info("Custom Successfully retrieved {} products for category {} with tags {} [RequestID: {}, TraceID: {}]",
-                products.size(), category, tags, requestId, traceId);
-
-            return products;
+//            return products;
         } catch (FeignException fe) {
             log.error("Feign error retrieving products [RequestID: {}, TraceID: {}, Category: {}, HTTP: {}, Message: {}]",
                     requestId, traceId, category, fe.status(), fe.getMessage(), fe);
